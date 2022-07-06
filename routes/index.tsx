ab7b24f14822ecb596c0bdcaf3afdbe099f23fa0
Markaronin <jmarkroylance@gmail.com>
2022-07-05 19:22:10 -0600

    Don't flash on mobile page load

--- routes/index.tsx
+++ routes/index.tsx
@@ -20,39 +20,41 @@ export default function Home({ data }: PageProps<Record<string, Todo>>) {
                 <link rel="stylesheet/less" href="style.less" />
                 <script src="https://cdn.jsdelivr.net/npm/less@4"></script>
             </Head>
-            <div>
-                <h1>To Talk About</h1>
-                {Object.entries(data)
-                    .sort((a, b) => a[1].title.localeCompare(b[1].title))
-                    .map(([id, todo]) => (
-                        <div className="todo-row">
-                            <form
-                                method="post"
-                                action="api/todo"
-                                style={{ display: "flex" }}
-                            >
-                                <input type="hidden" name="deleting" />
-                                <input type="hidden" name="id" value={id} />
-                                {todo.title}
-                                <button style={{ marginLeft: "auto" }}>
-                                    Delete
-                                </button>
-                            </form>
-                        </div>
-                    ))}
-                <hr />
-                <form method="post" action="api/todo">
-                    Add new TODO:
-                    <br />
-                    <label>
-                        Title:
-                        <input name="title" />
-                    </label>
-                    <br />
-                    <button>Add</button>
-                </form>
-                <hr />
-            </div>
+            <body style={{ backgroundColor: "#272727" }}>
+                <div>
+                    <h1>To Talk About</h1>
+                    {Object.entries(data)
+                        .sort((a, b) => a[1].title.localeCompare(b[1].title))
+                        .map(([id, todo]) => (
+                            <div className="todo-row">
+                                <form
+                                    method="post"
+                                    action="api/todo"
+                                    style={{ display: "flex" }}
+                                >
+                                    <input type="hidden" name="deleting" />
+                                    <input type="hidden" name="id" value={id} />
+                                    {todo.title}
+                                    <button style={{ marginLeft: "auto" }}>
+                                        Delete
+                                    </button>
+                                </form>
+                            </div>
+                        ))}
+                    <hr />
+                    <form method="post" action="api/todo">
+                        Add new TODO:
+                        <br />
+                        <label>
+                            Title:
+                            <input name="title" />
+                        </label>
+                        <br />
+                        <button>Add</button>
+                    </form>
+                    <hr />
+                </div>
+            </body>
         </Fragment>
     );
 }
